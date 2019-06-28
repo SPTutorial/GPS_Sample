da257c2d2cb777f1eaefcdc3de42f49f4be72b16
Subrata Pal <sub.pal28@gmail.com>
2019-06-28 22:44:25 +0530

    Add project files.

---
+++ GPS_Sample/GPS_Sample/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Plugin.Geolocator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GPS_Sample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void GetPosition(object sender, EventArgs e)
+        {
+            if(CrossGeolocator.Current.IsGeolocationAvailable)
+            {
+                if(CrossGeolocator.Current.IsGeolocationEnabled)
+                {
+                    var locator = CrossGeolocator.Current;
+                    locator.DesiredAccuracy = 50;
+
+                    var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(10));
+
+                    Device.BeginInvokeOnMainThread(() => {
+                        latitude.Text = $"Latitude : {position.Latitude}";
+                        longitude.Text = $"Longitude : {position.Longitude}";
+                        accuracy.Text = $"Accuracy : {Convert.ToInt32( position.Accuracy)} Meter";
+                    });
+                }
+                else
+                {
+                    await DisplayAlert("Message", "GPS Not Enabled", "Ok");
+                }
+            }
+            else
+            {
+                await DisplayAlert("Message", "GPS Not Available", "Ok");
+            }
+        }
+    }
+}
